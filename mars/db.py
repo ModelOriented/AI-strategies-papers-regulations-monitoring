896b64cc3a6835e9c74594e662f7cec8b04a690f
Stanislaw Gizinski <s.gizinski84@gmail.com>
2021-07-20 21:50:06 +0200

    document adding functions added

---
+++ mars/db.py
@@ -0,0 +1,48 @@
+import uuid
+from enum import Enum
+from shutil import copyfile
+
+from pyArango.connection import Connection
+
+URL = "url"
+FILENAME = "filename"
+FILE_TYPE = "file_type"
+SOURCE = "source_website"
+
+
+class SourceWebsite(str, Enum):
+    oecd = "oecd"
+
+
+class FileType(str, Enum):
+    pdf = "pdf"
+    html = "html"
+
+
+conn = Connection(username="root", password="rootpassword")
+
+db = conn.createDatabase(name="mars")
+documentSources = db.createCollection(name="Documents")
+
+
+def is_document_present(url: str) -> bool:
+    """Checks if documents from given url is downloaded"""
+    return len(documentSources.fetchFirstExample({URL: url})) == 1
+
+
+def save_doc(url: str, file, file_type: FileType, source: SourceWebsite) -> None:
+    """Saves new source document to database"""
+    file_name = _new_file(file_type, file)
+    doc = documentSources.createDocument()
+
+    doc[URL] = url
+    doc[FILE_TYPE] = str(file_type)
+    doc[FILENAME] = file_name
+    doc[SOURCE] = str(source)
+    doc.save()
+
+
+def _new_file(file, file_type: str):
+    filename = str(uuid.uuid4()) + file_type
+    copyfile(file, filename)
+    return filename
