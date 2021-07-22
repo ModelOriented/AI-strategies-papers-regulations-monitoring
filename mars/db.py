47a1de32575606561dd42d40d8bd676a5fe8a95d
Stanislaw Gizinski <s.gizinski84@gmail.com>
2021-07-22 15:43:12 +0200

    extracted text saving functions

--- mars/db.py
+++ mars/db.py
@@ -8,6 +8,9 @@ URL = "url"
 FILENAME = "filename"
 FILE_TYPE = "file_type"
 SOURCE = "source_website"
+DOC_ID = "source_doc_id"
+CONTENT = "content"
+EXTRACTION_METHOD = "extraction_method"
 
 FILES_DIR = "raw_data"
 
@@ -21,10 +24,26 @@ class FileType(str, Enum):
     html = "html"
 
 
-conn = Connection(username="root", password="rootpassword")
+class ExtractionMetod(str, Enum):
+    newspaper = "newspaper3k"
+    dragnet = "dragnet"
 
-db = conn.createDatabase(name="mars")
-documentSources = db.createCollection(name="Documents")
+
+def get_collection_or_create(db, collection_name: str):
+    try:
+        return db[collection_name]
+    except KeyError:
+        return db.createCollection(name=collection_name)
+
+
+conn = Connection(username="root", password="rootpassword")
+try:
+    db = conn.databases["mars"]
+except KeyError:
+    db = conn.createDatabase("mars")
+
+documentSources = get_collection_or_create(db, "Documents")
+contents = get_collection_or_create(db, "Texts")
 
 
 def is_document_present(url: str) -> bool:
@@ -43,6 +62,22 @@ def save_doc(
     doc[FILE_TYPE] = str(file_type)
     doc[FILENAME] = file_name
     doc[SOURCE] = str(source)
+    doc.save()
+
+
+def save_extracted_content(
+    source_url: str, content: str, extraction_method: ExtractionMetod
+) -> None:
+    """Saves extracted content to database"""
+    doc = contents.createDocument()
+    try:
+        id = documentSources.fetchFirstExample({URL: source_url})[0]._id
+    except IndexError:
+        raise Exception("Document not found")
+
+    doc[DOC_ID] = id
+    doc[CONTENT] = content
+    doc[EXTRACTION_METHOD] = str(extraction_method)
     doc.save()
 
 
