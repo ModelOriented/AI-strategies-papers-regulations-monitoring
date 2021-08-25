e68fe00e7b8f4b37074f9748e9a5ade6a3af0844
Jakub Wiśniewski <jakwisn@gmail.com>
2021-08-25 14:31:38 +0200

    extracting jobin2019

--- mars/utils.py
+++ mars/utils.py
@@ -1,6 +1,10 @@
 import json
 import os
 import ssl
+import pdfminer.converter
+import pdfminer.layout
+import pdfminer.pdfinterp
+import pdfminer.pdfpage
 
 import pandas as pd
 import requests
@@ -65,3 +69,41 @@ def get_number_of_files(dir: str):
     else:
         n_files = 0
     return n_files
+
+
+
+# extract text from PDF
+
+def extract_text_from_pdf(file_name):
+    """Extract text and other attributes from pdf in form od dict"""
+    empty_pages = []
+    separated_text = []
+    all_text = ""
+    page_no = 0
+    document = open(file_name, "rb")
+    rsrcmgr = pdfminer.pdfinterp.PDFResourceManager()
+    laparams = pdfminer.layout.LAParams()
+    device = pdfminer.converter.PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = pdfminer.pdfinterp.PDFPageInterpreter(rsrcmgr, device)
+    for page in pdfminer.pdfpage.PDFPage.get_pages(document):
+        text_on_page = []
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for element in layout:
+            if isinstance(element, pdfminer.layout.LTTextBoxHorizontal):
+                text_on_page.append(element.get_text())
+                all_text += element.get_text()
+        if len(text_on_page) == 0:
+            empty_pages.append(page_no)
+        separated_text.append(text_on_page)
+        page_no += 1
+    document.close()
+
+    document_dict = {
+        'all_text': all_text,
+        'text_on_page': text_on_page,
+        'empty_pages': empty_pages,
+        'page_no': page_no,
+        'separated_text': separated_text
+    }
+    return document_dict
