ab3cf81b4024129e816216768682342c67dcd2fb
Jakub Wiśniewski <jakwisn@gmail.com>
2021-09-16 12:39:10 +0200

    intelligent link fetching

--- mars/utils.py
+++ mars/utils.py
@@ -1,12 +1,17 @@
 import json
 import os
+import re
 import ssl
+import time
+import urllib
 
 import pdfminer.converter
 import pdfminer.layout
 import pdfminer.pdfinterp
 import pdfminer.pdfpage
 import requests
+import undetected_chromedriver as uc
+from googlesearch import search
 
 from mars.db import db_fields
 
@@ -106,3 +111,144 @@ def extract_text_from_pdf(file_name: str) -> dict:
         "separated_text": separated_text,
     }
     return document_dict
+
+
+def search_for_url(string: str):
+    """
+    Searches for url in a string. If a string has query inside than returns aforementioned url. If not than
+    returns None
+    """
+    result = re.search(
+        "(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?",
+        string,
+    )
+    if result is not None:
+        return result.group()
+    else:
+        return None
+
+
+def get_google_first_result_bs(query):
+    """
+    Omits results with from google scholar, unfortunately id does not omit adds.
+    Uses library based on beautifulsoup. After some searches it might be detected.
+    """
+    query = urllib.parse.quote_plus(query)
+    result = [None, None]
+    for i, val in enumerate(search(query, num=2, stop=2)):
+        result[i] = val
+    if result[0][:22] != "https://scholar.google":
+        return result[0]
+    else:
+        return result[1]
+
+
+def get_google_first_result_selenium(query):
+    """
+    Uses undetected chrome browser to query google and fetch the first link. Omits google
+    scholar. Does not omit adds.
+    Only limited number of searches is available, until it gives capcha.
+    """
+    query = urllib.parse.quote_plus(query)
+
+    def search_for_item(query, which):
+        driver = uc.Chrome()
+        driver.get("https://www.google.com/search?q=" + query + "&start=" + str(0))
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="L2AGLb"]').click()
+        item = driver.find_elements_by_css_selector("h3")[which]
+        item.click()
+        time.sleep(2)
+        result = driver.current_url
+
+        driver.quit()
+        return result
+
+    result = search_for_item(query, 0)
+
+    if result[:22] == "https://scholar.google":
+        result = search_for_item(query, 1)
+
+    return result
+
+
+def get_duckduckgo_first_result(driver, query: str) -> str:
+    """
+    Gets url from the first (top) result of duckduckgo.
+    @param driver: selenium driver, advised to use undetected-chromedriver
+    @param query: some query to be searched, it might be plain text.
+    @return: single url
+    """
+    query = urllib.parse.quote_plus(query)
+    driver.get(f"https://duckduckgo.com/?q={query}&t=h_&ia=web")
+    result = driver.find_element_by_class_name("result__a").get_attribute("href")
+    return result
+
+
+def get_inteligent_first_search_results(queries: list) -> dict:
+    """
+    Intelligently searches for a list of queries using
+    BeautifulSoup (bs) + google, Selenium + google and Selenium + DuckDuckGo.
+    If one component fails switches to the next one. So if both Selenium and BeautifulSoup
+    fail with google, Selenium will continue with DuckDuckGo
+    @param queries: list of queries
+    @return: dict with matches and the search method
+    """
+    basic_google_down = False
+    selenium_google_down = False
+
+    duck_duck_driver = False
+
+    results = {}
+    # first basic search:
+    for query in queries:
+
+        if not basic_google_down:
+            try:
+                time.sleep(2)
+                result = (get_google_first_result_bs(query), "beautifulsoupgoogle")
+
+            except:
+                basic_google_down = True
+                print("BS google is down")
+
+        if not selenium_google_down and basic_google_down:
+            try:
+                result = (get_google_first_result_selenium(query), "seleniumgoogle")
+            except:
+                selenium_google_down = True
+
+                print("Selenium Google is down")
+
+        if selenium_google_down and basic_google_down:
+            if duck_duck_driver is False:
+                duck_duck_driver = uc.Chrome()
+
+            result = (
+                get_duckduckgo_first_result(duck_duck_driver, query),
+                "duckduckgo",
+            )
+
+        results[query] = result
+
+    return results
+
+
+import time
+import semanticscholar as sch
+
+
+def fetch_paper_information(arxiv_id: str):
+    """Fetches paper information based on semantic scholar api"""
+    print("Fetching: {}".format(arxiv_id))
+    time.sleep(2.5)
+    paper = sch.paper(
+        "arXiv:{}".format(arxiv_id), timeout=10, include_unknown_references=True
+    )
+    try:
+        return (
+            paper,
+            [p["arxivId"] for p in paper["references"] if p["arxivId"] is not None],
+        )
+    except KeyError:
+        return paper, []
---
+++ scripts/parse_jobin2019.py
@@ -0,0 +1,227 @@
+import pdfminer
+import numpy as np
+import pdfminer.converter
+import pdfminer.layout
+import pdfminer.pdfinterp
+import pdfminer.pdfpage
+import re
+import pandas as pd
+from mars.utils import (
+    extract_text_from_pdf,
+    get_inteligent_first_search_results,
+    fetch_paper_information,
+)
+from mars.utils import search_for_url
+import typer
+import os
+
+
+def get_longest(text_list: list) -> list:
+    lengths = []
+    for text in text_list:
+        lengths.append(len(text))
+    lengths = np.array(lengths)
+    return text_list[int(np.argmax(lengths))]
+
+
+def extract_references_from_jobin2019(file_name):
+    def split(txt, seps):
+        default_sep = seps[0]
+
+        # we skip seps[0] because that's the default separator
+        for sep in seps[1:]:
+            txt = txt.replace(sep, default_sep)
+        return [i.strip() for i in txt.split(default_sep)]
+
+    text = extract_text_from_pdf(file_name)["all_text"]
+    text = text.split("REFERENCES")[1]
+    text = text.split("Supplementary Information")[0]
+    separators = re.findall("\\n\d+\.", text)
+    references = split(text, separators)
+
+    return references
+
+
+def match_references_substring(ref1: list, ref2: list) -> dict:
+    """
+
+    From two lists of references makes a dict with pairs of matching indices.
+    The matching is done in
+    """
+    refs = {}
+    for i in range(1, len(ref1)):
+        r1 = ref1[i]
+        refs[i] = None
+        for j, r2 in enumerate(ref2):
+            if r2.lower() in r1.lower():
+                refs[i] = j
+
+    return refs
+
+
+def extract_citations_from_jobin2019(file_name: str) -> dict:
+    """Extracts citation numbers from jobin2019 - preprint version"""
+
+    # get text
+    separated_text = extract_text_from_pdf(file_name)["separated_text"]
+
+    # getting the longest - the proper text on page
+    text_on_pages = [get_longest(st) for st in separated_text]
+    text_on_pages = np.array(text_on_pages)
+
+    # split words like buckets
+    # everything above justice fairness and equity is Transparency
+    # later it is excluded from text and everything above Non-maleficience is Justice, fairness, and equity, etc.
+    split_words = [
+        "Transparency",
+        "Justice, fairness, and equity",
+        "Non-maleficence",
+        "Responsibility and accountability",
+        "Privacy",
+        "Beneficence",
+        "Freedom and autonomy",
+        "Trust",
+        "Sustainability",
+        "Dignity",
+        "Solidarity",
+        "Discussion",
+    ]  # stopword - last one will be excluded
+
+    threads = dict.fromkeys(split_words)
+
+    # clean
+    for i, text in enumerate(text_on_pages):
+        text = text.replace("\n", "")
+        text = text.replace("(cf. Table 2)", "")
+        text = text.replace("1.5", "")
+
+        text_on_pages[i] = text
+
+    relevant_text = " ".join(text_on_pages[7:13])
+
+    for i in range(len(list(threads.keys())) - 1):
+        earlier_thread = list(threads.keys())[i]
+        split_thread = list(threads.keys())[i + 1]
+        splitted = relevant_text.split(split_thread, 1)
+        threads[earlier_thread] = splitted[0]
+
+        relevant_text = splitted[1]
+
+    # pop empty one
+    threads.pop("Discussion")
+
+    all_citations = dict.fromkeys(list(threads.keys()))
+    for key, text in threads.items():
+
+        citations = np.array(re.findall(r"\d+", text))
+        additional_citations = re.findall(r"\d+[–]\d+", text)
+        for ac in additional_citations:
+            range_ = re.findall(r"\d+", ac)
+            lower, higher = int(range_[0]), int(range_[1])
+            between = np.arange(lower + 1, higher)
+            citations = np.append(citations, between)
+        all_citations[key] = citations
+
+    for key, ac in all_citations.items():
+        ac = np.unique(ac)
+        all_citations[key] = ac
+
+    return all_citations
+
+
+def match_references_with_url(file_name: str, arxiv_id: str) -> dict:
+    """
+    Matches references from jobin2019 and version from semantic scholar to obtain urls.
+    If no url is found it searches google and duckduckgo using inteligent search.
+    @param file_name: string, filename of jobin2019
+    @param arxiv_id: string, arxiv id of jobin2018
+    @return: returns dict with matches and information about where the match was found
+    """
+    references = extract_references_from_jobin2019(file_name)
+
+    for i, ref in enumerate(references):
+        references[i] = ref.replace("\n", "")
+
+    paper_info = fetch_paper_information(arxiv_id)
+    references_scholar = paper_info[0]["references"]
+
+    references_scholar_titles = [rs["title"] for rs in references_scholar]
+
+    refs = match_references_substring(references, references_scholar_titles)
+
+    links = {}
+
+    for i, j in refs.items():
+
+        try:
+            if refs[i] is not None:
+                links[i] = references_scholar[j]["url"]
+            else:
+                links[i] = search_for_url(references[i])
+
+            if links[i] == "" or links[i] is None:
+                links[i] = search_for_url(references[i])
+                if links[i] is None:
+                    raise Exception
+        except:
+            links[i] = None
+
+    unresolved_links = {}
+    for k, v in links.items():
+        if v is None:
+            unresolved_links[k] = references[k]
+
+    results = get_inteligent_first_search_results(list(unresolved_links.values()))
+
+    i = 0
+    for link_id, value in unresolved_links.items():
+        links[link_id] = list(results.values())[i]
+        i += 1
+
+    return links
+
+
+def extract_topics_from_jobin_citations(path_to_jobin_file: str) -> pd.DataFrame:
+    """Extracts topics for each relevant citation in jobin2019 preprint version"""
+    citations = extract_citations_from_jobin2019(path_to_jobin_file)
+
+    all_citations = np.array([])
+    for c in citations.values():
+        all_citations = np.append(all_citations, c)
+
+    all_citations = np.sort(np.unique(all_citations).astype(int))
+
+    citations_dict = dict.fromkeys(all_citations)
+
+    for k in citations_dict.keys():
+        citations_dict[k] = np.zeros(11)
+
+    mapping = {
+        0: "Transparency",
+        1: "Justice, fairness, and equity",
+        2: "Non-maleficence",
+        3: "Responsibility and accountability",
+        4: "Privacy",
+        5: "Beneficence",
+        6: "Freedom and autonomy",
+        7: "Trust",
+        8: "Sustainability",
+        9: "Dignity",
+        10: "Solidarity",
+    }
+
+    for key1, val1 in citations_dict.items():
+        for key2, val2 in mapping.items():
+            if np.isin(key1, citations[val2]):
+                citations_dict[key1][key2] = 1
+
+    data = pd.DataFrame(citations_dict).T.astype(int)
+    data.columns = mapping.values()
+
+    return data
+
+
+if __name__ == "__main__":
+    file_name = os.environ["JOBIN2019_LOCATION"]  # change to your location
+    # ToDo make a script and upload to database
+    # typer.run()
