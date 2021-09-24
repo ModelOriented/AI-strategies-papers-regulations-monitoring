6945923be75ea8866ba7f02d6b6672b5f2b245cf
Stanisław Giziński <s.gizinski84@gmail.com>
2021-09-24 16:04:27 +0200

    rename app

--- reporting_app.py
+++ reporting_app.py
@@ -27,16 +27,3 @@ st.bar_chart(hist_values[0])
 st.write(data)
 print(hist_values)
 st.info("This is a purely informational message")
-
-# # Create a text element and let the reader know the data is loading.
-# data_load_state = st.text("Loading data...")
-# # Load 10,000 rows of data into the dataframe.
-# data = load_data(10000)
-# # Notify the reader that the data was successfully loaded.
-# data_load_state.text("Loading data...done!")
-# st.subheader("Number of pickups by hour")
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-# st.bar_chart(hist_values)
-# if st.checkbox("Show raw data"):
-#     st.subheader("Raw data")
-#     st.write(data)
