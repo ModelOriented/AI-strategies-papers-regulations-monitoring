12f7556e2c593cb1dc96e1ec170150ce426d9e35
Hanna Zdulska <zdula616@gmail.com>
2021-09-28 21:24:47 +0200

    add docs

--- mars/segmentation/pdf_segmentation.py
+++ mars/segmentation/pdf_segmentation.py
@@ -1,14 +1,15 @@
-import fitz
 import re
 from operator import itemgetter
 
+import fitz
 
-def fonts(doc, round_digits=1) -> tuple[dict, dict]:
+
+def count_fonts(doc: fitz.fitz.Document, round_digits: int = 1) -> Tuple[dict, dict]:
     """Extracts fonts and their usage in PDF documents.
     :param doc: PDF document to iterate through
     :type doc: <class 'fitz.fitz.Document'>
     :rtype: [(font_size, count), (font_size, count}], dict
-    :return: most used fonts sorted by count, font style information
+    :return: most used count_fonts sorted by count, font style information
     """
     styles = {}
     font_counts = {}
@@ -27,19 +28,21 @@ def fonts(doc, round_digits=1) -> tuple[dict, dict]:
 
                         font_counts[identifier] = (
                             font_counts.get(identifier, 0) + 1
-                        )  # count the fonts usage
+                        )  # count the count_fonts usage
 
     font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
 
     if len(font_counts) < 1:
-        raise ValueError("Zero discriminating fonts found!")
+        raise ValueError("Zero discriminating count_fonts found!")
 
     return font_counts, styles
 
 
-def font_tags(font_counts, styles, round_digits=1) -> dict:
+def translate_font_to_tags(
+    font_counts: dict, styles: dict, round_digits: int = 1
+) -> dict:
     """Returns dictionary with font sizes as keys and tags as value.
-    :param font_counts: (font_size, count) for all fonts occuring in document
+    :param font_counts: (font_size, count) for all count_fonts occuring in document
     :type font_counts: list
     :param styles: all styles found in the document
     :type styles: dict
@@ -73,7 +76,7 @@ def font_tags(font_counts, styles, round_digits=1) -> dict:
     return size_tag
 
 
-def headers_para(doc, size_tag, round_digits=1):
+def tag_headers(doc: fitz.fitz.Document, size_tag: dict, round_digits: int = 1):
     """Scrapes headers & paragraphs from PDF and return texts with element tags.
     :param doc: PDF document to iterate through
     :type doc: <class 'fitz.fitz.Document'>
@@ -139,7 +142,6 @@ def headers_para(doc, size_tag, round_digits=1):
 def merge_spans(header_para):
 
     not_empty = [h for h in header_para if h]
-
     closed = []
 
     # fix to avoid errors out of range
@@ -180,14 +182,14 @@ def merge_spans(header_para):
 def segment_pdf(filename, round_digits=1):
     """
     splits pdf file into headers and paragraphs
-    @param round_digits - how many digits should be rounded while counting fonts
+    @param round_digits - how many digits should be rounded while counting count_fonts
 
     @returns list of dicts {"html_tag": string, "content": string}
     """
     doc = fitz.open(filename=filename)
-    font_counts, styles = fonts(doc, round_digits=round_digits)
-    size_tag = font_tags(font_counts, styles, round_digits=round_digits)
-    arr = headers_para(doc, size_tag, round_digits=round_digits)
+    font_counts, styles = count_fonts(doc, round_digits=round_digits)
+    size_tag = translate_font_to_tags(font_counts, styles, round_digits=round_digits)
+    arr = tag_headers(doc, size_tag, round_digits=round_digits)
     segments = merge_spans(arr)
 
     return segments
