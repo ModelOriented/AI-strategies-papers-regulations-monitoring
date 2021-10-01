8a2d58748c7971ef4da31862474108de46d506e1
Stanisław Giziński <s.gizinski84@gmail.com>
2021-10-01 16:43:27 +0200

    Add save batching

--- scripts/assign_languages_to_sections.py
+++ scripts/assign_languages_to_sections.py
@@ -1,3 +1,5 @@
+"""Gets all segments from database without assigned language, recognize their language and saves it to database."""
+
 import typer
 from mars import logging
 from mars.db import collections
@@ -6,10 +8,13 @@ from mars.language_recognition import detect_language
 from tqdm import tqdm
 
 logger = logging.new_logger(__name__)
+BATCH_SIZE = 1000
 
 
 def main():
-    query = collections.segmented_texts.fetchByExample({LANGUAGE: None}, batchSize=100)
+    query = collections.segmented_texts.fetchByExample(
+        {LANGUAGE: None}, batchSize=BATCH_SIZE
+    )
     print(f"Processing {query.count} sentences...")
     sections_to_update = []
     for section in tqdm(query, total=query.count):
@@ -17,10 +22,13 @@ def main():
             lang = detect_language(section[CONTENT])
             section[LANGUAGE] = lang
             sections_to_update.append(section)
+            if len(sections_to_update) == BATCH_SIZE:
+                collections.segmented_texts.bulkSave(
+                    sections_to_update, onDuplicate="update"
+                )
+                sections_to_update = []
         except Exception as e:
             logging.log_exception("Exception:", e, logger)
-    print("Updating...")
-    collections.segmented_texts.bulkSave(sections_to_update, onDuplicate="update")
 
 
 if __name__ == "__main__":
