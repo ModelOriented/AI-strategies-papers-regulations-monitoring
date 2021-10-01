f56c4fb76d44bbbb6ca166ff53a05ccc18e63c85
Hanna Zdulska <zdula616@gmail.com>
2021-10-01 11:49:49 +0200

    spelling

--- mars/segmentation/pdf_segmentation.py
+++ mars/segmentation/pdf_segmentation.py
@@ -5,11 +5,11 @@ import fitz
 
 
 def count_fonts(doc: fitz.fitz.Document, round_digits: int = 1) -> Tuple[dict, dict]:
-    """Extracts count_fonts and their usage in PDF documents.
+    """Extracts fonts and their usage in PDF documents.
     :param doc: PDF document to iterate through
     :type doc: <class 'fitz.fitz.Document'>
     :rtype: [(font_size, count), (font_size, count}], dict
-    :return: most used count_fonts sorted by count, font style information
+    :return: most used fonts sorted by count, font style information
     """
     styles = {}
     font_counts = {}
@@ -28,12 +28,12 @@ def count_fonts(doc: fitz.fitz.Document, round_digits: int = 1) -> Tuple[dict, d
 
                         font_counts[identifier] = (
                             font_counts.get(identifier, 0) + 1
-                        )  # count the count_fonts usage
+                        )  # count the fonts usage
 
     font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
 
     if len(font_counts) < 1:
-        raise ValueError("Zero discriminating count_fonts found!")
+        raise ValueError("Zero discriminating fonts found!")
 
     return font_counts, styles
 
@@ -42,7 +42,7 @@ def translate_font_to_tags(
     font_counts: dict, styles: dict, round_digits: int = 1
 ) -> dict:
     """Returns dictionary with font sizes as keys and tags as value.
-    :param font_counts: (font_size, count) for all count_fonts occuring in document
+    :param font_counts: (font_size, count) for all fonts occuring in document
     :type font_counts: list
     :param styles: all styles found in the document
     :type styles: dict
@@ -182,7 +182,7 @@ def merge_spans(header_para):
 def segment_pdf(filename, round_digits=1):
     """
     splits pdf file into headers and paragraphs
-    @param round_digits - how many digits should be rounded while counting count_fonts
+    @param round_digits - how many digits should be rounded while counting fonts
 
     @returns list of dicts {"html_tag": string, "content": string}
     """
