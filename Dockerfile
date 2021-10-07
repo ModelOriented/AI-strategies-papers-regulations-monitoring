9a2408dca9d4c07b6c9260bc489350d2c1b2635e
Hanna Zdulska <43793964+HaZdula@users.noreply.github.com>
2021-10-07 14:18:34 +0200

    Feature/bump python (#25)
    * bump python
    
    * silence unsecure warnings
    
    * Remove geckodriver from repo and add downloading to install script
    
    * Add missing logging level env variables
    
    * Update OECD api url
    
    * Add .python-version
    
    Co-authored-by: Piotr Piatyszek <piotrp@wektor.xyz>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.11
+FROM python:3.8
 ENV DEBIAN_FRONTEND=noninteractive
 WORKDIR /mair
 
--- install_prerequirements.sh
+++ install_prerequirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install firefox -y
-sudo apt-get install libxml2-dev
+sudo apt install firefox libxml2 -y
+wget -N https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz && tar xzvf geckodriver-v0.30.0-linux64.tar.gz && rm geckodriver-v0.30.0-linux64.tar.gz
