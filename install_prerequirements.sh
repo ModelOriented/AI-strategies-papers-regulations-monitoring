74d08f6b1758d83c3045272ccfcf9312a4370127
Piotr Piatyszek <piotrp@wektor.xyz>
2021-10-09 23:38:22 +0000

    install google chrome

--- install_prerequirements.sh
+++ install_prerequirements.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 apt-get update && apt-get install -y --no-install-recommends freetds-bin krb5-user ldap-utils libffi7 libsasl2-2 libsasl2-modules libssl1.1 locales lsb-release sasl2-bin sqlite3 unixodbc firefox-esr libxml2 unzip
-wget -N https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz && tar xzvf geckodriver-v0.30.0-linux64.tar.gz && rm geckodriver-v0.30.0-linux64.tar.gz
-wget https://chromedriver.storage.googleapis.com/95.0.4638.17/chromedriver_linux64.zip -O chromedriver.zip && unzip chromedriver.zip && rm chromedriver.zip
+# geckodriver
+wget -N https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
+tar xzvf geckodriver-v0.30.0-linux64.tar.gz
+rm geckodriver-v0.30.0-linux64.tar.gz
+# chrome driver
+wget https://chromedriver.storage.googleapis.com/95.0.4638.17/chromedriver_linux64.zip -O chromedriver.zip
+unzip chromedriver.zip
+rm chromedriver.zip
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+dpkg -i chrome.deb
+rm chrome.deb
