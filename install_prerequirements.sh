630344b1a9553592edc783e79377a36ee3da1e33
Jakub Wiśniewski <jakwisn@gmail.com>
2021-10-11 13:43:12 +0200

    add chrome to prerequirements

--- install_prerequirements.sh
+++ install_prerequirements.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo apt install firefox libxml2 -y
 wget -N https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz && tar xzvf geckodriver-v0.30.0-linux64.tar.gz && rm geckodriver-v0.30.0-linux64.tar.gz
+sudo apt install gdebi-core
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
