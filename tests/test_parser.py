693daa50e12f11695acc44d97e17a875cb77bdc0
Jakub Wiśniewski <jakwisn@gmail.com>
2021-10-18 18:36:43 +0200

    change path

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ from mars.parser import extract_text_from_pdf
 class Parser(unittest.TestCase):
 
     def test_extract_text_from_pdf(self):
-        result = extract_text_from_pdf('test_document.pdf')
+        result = extract_text_from_pdf('tests/test_document.pdf')
         self.assertEqual(result['all_text'].strip(), 'test')
 
 if __name__ == "__main__":
