1aa3d610524fb8826ac6f688b3ac0214c1f6caab
Hanna Zdulska <zdula616@gmail.com>
2021-11-05 10:00:14 +0100

    add better logging

--- scripts/eurlex-metadata-scraping.py
+++ scripts/eurlex-metadata-scraping.py
@@ -10,22 +10,24 @@ logger.setLevel(logging.getLevelName(config.logging_level))
 
 url = "http://publications.europa.eu/resource/celex/%s?language=%s"
 
-for doc in collections.document_sources.fetchByExample(
-    {db_fields.SOURCE: db_fields.SourceWebsite.eurlex}, batchSize=1000
+for doc in list(
+    collections.document_sources.fetchByExample(
+        {db_fields.SOURCE: db_fields.SourceWebsite.eurlex, db_fields.TITLE: None},
+        batchSize=1000,
+    )
 ):
     try:
-        if not doc[db_fields.TITLE]:
-            url_celex = url % (doc["celex"], doc["lang"])
-            r = requests.get(
-                url_celex, headers={"Accept": "application/xml;notice=object"}
-            )
-            r.raise_for_status()
-            root = etree.fromstring(r.content)
+        url_celex = url % (doc["celex"], doc["lang"])
+        r = requests.get(url_celex, headers={"Accept": "application/xml;notice=object"})
+        r.raise_for_status()
+        root = etree.fromstring(r.content)
 
-            doc[db_fields.COUNTRY] = root.find(".//CREATED_BY/PREFLABEL").text
-            doc[db_fields.TITLE] = root.find(".//EXPRESSION_TITLE/VALUE").text
-            doc[db_fields.START_DATE] = root.find(".//WORK_DATE_DOCUMENT/VALUE").text
-            doc.patch()
+        doc[db_fields.COUNTRY] = root.find(".//CREATED_BY/PREFLABEL").text
+        doc[db_fields.TITLE] = root.find(".//EXPRESSION_TITLE/VALUE").text
+        doc[db_fields.START_DATE] = root.find(".//WORK_DATE_DOCUMENT/VALUE").text
+        doc.patch()
 
     except Exception as e:
-        logging.log_exception("", e, logger)
+        logging.log_exception(
+            "Failed to fetch metadata for CELEX: %s" % doc["celex"], e, logger
+        )
