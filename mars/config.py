aa3dc93198868812adea399d0b1ef9d84f5030d6
Piotr Piatyszek <piotrp@wektor.xyz>
2021-11-08 21:51:22 +0000

    Add redis & cleanup

--- mars/config.py
+++ mars/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 # Docker environment
 load_dotenv("/run/secrets/arango_secrets")
 load_dotenv("/run/secrets/webdav_secrets")
+load_dotenv("/run/secrets/redis_secrets")
 
 logging_level = os.getenv("LOGGING_LEVEL")
 scrapper_logs_dir = os.getenv("SCRAPER_LOGS_DIR")
@@ -22,3 +23,4 @@ webdav_login = os.getenv("WEBDAV_LOGIN") or "user"
 webdav_password = os.getenv("WEBDAV_PASSWORD")
 webdav_url = os.getenv("WEBDAV_ENDPOINT")
 use_webdav = (os.getenv("USE_WEBDAV") or "0") in ["1", "True", "true", "TRUE"]
+redis_password = os.getenv("REDIS_PASSWORD")
