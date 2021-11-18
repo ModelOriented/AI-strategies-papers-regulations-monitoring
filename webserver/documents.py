0d163da4364b2c63b7b3dea6d9a6b26fef102f38
Jakub Wiśniewski <jakwisn@gmail.com>
2021-11-18 01:21:01 +0100

    corrected functions

--- webserver/documents.py
+++ webserver/documents.py
@@ -1,7 +1,7 @@
 import json
-import os
 
-from flask import Blueprint, request, Response, abort
+from flask import Blueprint, Response
+
 import mars
 from mars import config
 from mars.db import collections
@@ -9,71 +9,61 @@ from mars.db.db_fields import (
     SENTENCE,
     SEGMENT_ID,
     SENTENCE_DOC_ID,
-    SEGMENT_DOC_ID,
-    IS_DEFINITION,
-    ID,
-    KEY,
-    SEQUENCE_NUMBER,
-    SENTENCE_NUMBER
+    SENTENCE_NUMBER,
+    SEQUENCE_NUMBER
 )
-
 from mars.definition_model import DistilBertBaseUncased
+from typing import List
 
 blueprint = Blueprint('documents', __name__)
 
-def get_sentence_dict(key):
+
+def get_sentences(key: int)-> List:
+
     big_number = 1000000
 
-    get_segments = f"FOR u IN {collections.SEGMENTED_TEXTS} " \
-                   f"FILTER TO_NUMBER(SPLIT(u.{SEGMENT_DOC_ID}, \"/\")[1]) >= {key} " \
-                   f"&& TO_NUMBER(SPLIT(u.{SEGMENT_DOC_ID}, \"/\")[1]) <= {key}" \
-                   f"RETURN u"
-
     get_sentences = f"FOR u IN {collections.SENTENCES} " \
-                    f"FILTER TO_NUMBER(SPLIT(u.{SENTENCE_DOC_ID}, \"/\")[1]) >= {key} " \
-                    f"&& TO_NUMBER(SPLIT(u.{SENTENCE_DOC_ID}, \"/\")[1]) <= {key}" \
+                    f"FILTER TO_NUMBER(SPLIT(u.{SENTENCE_DOC_ID}, \"/\")[1]) == {key} " \
                     f"RETURN u"
 
     sentences = mars.db.database.AQLQuery(get_sentences, big_number)
-    segments = mars.db.database.AQLQuery(get_segments, big_number)
+    sentences_in_segment = []
+    for j, sentence in enumerate(sentences):
+        sentences_in_segment.append({SEQUENCE_NUMBER: sentence[SEQUENCE_NUMBER], SENTENCE_NUMBER: sentence[SENTENCE_NUMBER],
+                                     SENTENCE: sentence[SENTENCE]})
+    return sentences_in_segment
 
-    result = {}
-    for i, segment in enumerate(segments):
-        segment_ID = segment[ID]
-        sequence_number = segment[SEQUENCE_NUMBER]
 
-        sentences_in_segment = []
-        for j, sentence in enumerate(sentences):
-            if sentence[SEGMENT_ID] == segment_ID:
-                sentences_in_segment.append({SENTENCE_NUMBER: sentence[SENTENCE_NUMBER], SENTENCE: sentence[SENTENCE]})
-        result[sequence_number] = sentences_in_segment
-    return result
+@blueprint.route('/<int:key>/sentences')
+def get_status(key: int):
+    result = get_sentences(key)
+    result.sort(key=lambda x: (x[SEQUENCE_NUMBER], x[SENTENCE_NUMBER]))
 
-@blueprint.route('/<string:key>/sentences')
-def get_status(key):
+    d = dict().fromkeys([r[SEQUENCE_NUMBER] for r in result])
+    for s in result:
+        if d[s[SEQUENCE_NUMBER]] is None:
+            d[s[SEQUENCE_NUMBER]] = list()
+            d[s[SEQUENCE_NUMBER]].append(s[SENTENCE])
+        else:
+            d[s[SEQUENCE_NUMBER]].append(s[SENTENCE])
 
-    result = get_sentence_dict(key)
-    res = []
-    for seq in list(result.values()):
-        res.append([s['sentence'] for s in seq])
+    d = list(map(list, d.values()))
+    return Response(json.dumps(d))
 
-    return Response(json.dumps(res))
 
-@blueprint.route('/<string:key>/definitions')
-def get_definitions(key, path_to_model: str = "distilbert-base-uncased"):
-    #n = request.args.get('n') # Number of top definitions to return # TODO - nie działa (przynajmniej mi)
+@blueprint.route('/<int:key>/definitions')
+def get_definitions(key:int, path_to_model: str = "distilbert-base-uncased"):
+    # n = request.args.get('n') # Number of top definitions to return # TODO - nie działa (przynajmniej mi)
     n = 5
-    result = get_sentence_dict(key)
+    result = get_sentences(key)
     path_to_model = config.models_dir + '/' + path_to_model
     dbc = DistilBertBaseUncased(path_to_model)
 
-
     dict_list = []
-    for key, seq in result.items():
-        for sentence in seq:
-            dict_list.append({'segment': key,
-                              'sentence': sentence[SENTENCE_NUMBER],
-                              'probability': dbc.predict_single_sentence(sentence[SENTENCE])})
+    for sentence in result:
+        dict_list.append({'segment': sentence[SEQUENCE_NUMBER],
+                          'sentence': sentence[SENTENCE_NUMBER],
+                          'probability': dbc.predict_single_sentence(sentence[SENTENCE])})
 
     dict_list.sort(key=lambda x: x['probability'], reverse=True)
 
