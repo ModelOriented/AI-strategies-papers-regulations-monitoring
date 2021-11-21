d5439cd0ad0d47ac1a9c9fbe0029f2f7dc2e8e8a
Jakub Wiśniewski <jakwisn@gmail.com>
2021-11-21 21:18:02 +0100

    tokenizer saved

--- mars/definition_model.py
+++ mars/definition_model.py
@@ -15,27 +15,19 @@ class AbstractDefinitionModel(ABC):
 
 class DistilBertBaseUncased(AbstractDefinitionModel):
 
-    def __init__(self, path_to_model: str, transformer: str = "distilbert-base-uncased"):
+    def __init__(self, path_to_model: str = None, path_to_tokenizer: str = None,
+                 transformer: str = "distilbert-base-uncased"):
         self.model = TFDistilBertForSequenceClassification.from_pretrained(path_to_model)
-        self.tokenizer = AutoTokenizer.from_pretrained(transformer)
+        self.tokenizer = AutoTokenizer.from_pretrained(path_to_tokenizer)
 
     def predict_single_sentence(self, sentence: str) -> float:
-
-        def predict_tokens(tokens):
+        def predict_tokens(inputs):
             inputs["labels"] = tf.reshape(tf.constant(1), (-1, 1))  # Batch size 1
             outputs = self.model(inputs)
             predictions = tf.math.softmax(outputs.logits, axis=-1)
             return float(np.array(predictions)[0][1])
 
-        inputs = self.tokenizer(sentence, return_tensors="tf")
-        if len(inputs) > 512:
-            inputs_first = inputs[:512]
-            inputs_last = inputs[512:]
-            preds_first = predict_tokens(inputs_first)
-            preds_last = predict_tokens(inputs_last)
-            preds = float((preds_first + preds_last)/2)
-
-        else:
-            preds = predict_tokens(inputs)
+        inputs = self.tokenizer(sentence, return_tensors="tf", truncation=True)
+        preds = predict_tokens(inputs)
 
         return preds
--- tests/test_definition_scoring.py
+++ tests/test_definition_scoring.py
@@ -4,7 +4,8 @@ from mars import config
 from mars.definition_model import DistilBertBaseUncased
 
 path_to_model = config.models_dir + "/" + 'distilbert-base-uncased'
-dbu = DistilBertBaseUncased(path_to_model)
+path_to_tokenizer = config.models_dir + '/tokenizers/' + 'distilbert-base-uncased'
+dbu = DistilBertBaseUncased(path_to_model, path_to_tokenizer)
 
 
 class DefinitionScoring(unittest.TestCase):
@@ -16,7 +17,10 @@ class DefinitionScoring(unittest.TestCase):
         self.assertTrue(score > 0.5)
 
     def test_long_test(self):
-        text = "Participating insurance and reinsurance undertakings, insurance holding companies and mixed financial holding companies which, for the calculation of group solvency, use method 1 as defined in Article 230 of Directive 2009/138/EC, either exclusively or in combination with method 2 as defined in Article 233 of that Directive, shall submit annually the information referred to in Article 304(1)(d) of Delegated Regulation (EU) 2015/35, in conjunction with Article 372(1) of that Delegated Regulation, using the following templates in relation to all material ring-fenced funds and all material matching adjustment portfolios related to the part that is consolidated as referred to in points (a) and (c) of Article 335(1) of Delegated Regulation (EU) 2015/35, as well as in relation to the remaining part:"
+        with open("long_text.txt", 'r') as f:
+            text = f.readlines()
+            text = "\n".join(text).replace("\n", "")
+
         score = dbu.predict_single_sentence(text)
         self.assertIsInstance(score, float)
 
