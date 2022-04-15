1303c92f96a5d93a7bc993c10281f50e35e756ed
Stanisław Giziński <s.gizinski84@gmail.com>
2022-04-15 17:46:50 +0200

    spacy process s2orc

---
+++ scripts/s2orc/process_spacy_docs.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import spacy
+import joblib
+
+import mars.s2orc.loading
+
+OUT_PATH = 'data/s2orc/s2orc_ai_prefiltered_processed_with_doi.pkl'
+
+df = mars.s2orc.loading.load_s2orc_prefiltered()
+df2 = df[~df['doi'].isnull()]
+en = spacy.load('en_core_web_md')
+
+def process(text):
+    try:
+        return en(text)
+    except Exception:
+        print("Error: {}".format(text))
+        return []
+
+df2['doc'] = df2['abstract'].progress_apply(process)
+
+joblib.dump(df2, OUT_PATH)
