41f18593e1945a280bd8bd855ea277e584678fed
Stanisław Giziński <s.gizinski84@gmail.com>
2022-05-19 21:10:13 +0200

    clustering!

--- scripts/s2orc/cluster.py
+++ scripts/s2orc/cluster.py
@@ -12,12 +12,13 @@ import os
 tqdm.pandas()
 
 
-def main(in_table_path: str, in_json_path: str, out_path: str, n_jobs:int = -1, epsilons:List[float]=[.5]):
+def main(in_json_path: str, out_path: str, n_jobs:int = -1, epsilons:str=".5"):
     print("Loading data...")
-    df = pd.read_parquet(in_table_path)
-    print(len(df))
+    # df = pd.read_parquet(in_table_path)
+    # print(len(df))
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
-
+    epsilons = [float(epsilon) for epsilon in epsilons.split(",")]
+    print("Will process epsilons:", epsilons)
     print("Loading json...")
     chunk_to_embedding = json.load(open(in_json_path))
     print(len(chunk_to_embedding))
@@ -25,13 +26,13 @@ def main(in_table_path: str, in_json_path: str, out_path: str, n_jobs:int = -1, 
     for eps in epsilons:
         model = sklearn.cluster.DBSCAN(n_jobs=n_jobs, eps=eps)
         print("epsilon = ", eps)
-        print("Clutering chunks...")
+        # print("Clutering chunks...")
         clusters = model.fit_predict(list(chunk_to_embedding.values()))
-        print("Saving data...")
+        # print("Saving data...")
         df_out = pd.DataFrame([{"cluster": cluster, "chunk":chunk, "embedding":embedding} for cluster, (chunk, embedding) in zip(clusters, chunk_to_embedding.items())])
-        print("Outliers number:", sum(df_out['cluster']==-1))
+        print("Outliers number:", sum(df_out['cluster']==-1), "which is", sum(df_out['cluster']==-1)/len(df_out))
         print("Clusters number:", len(set(clusters)))
-        out_path_spec, ext = os.path.spliext(out_path)
+        out_path_spec, ext = os.path.splitext(out_path)
         df_out.to_parquet(out_path_spec+'_eps_'+str(eps)+ext)
 
 if __name__=='__main__':
