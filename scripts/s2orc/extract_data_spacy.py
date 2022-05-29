17fd0cbb6544197474cca1ddf2b2b950c53fe3db
Stanisław Giziński <s.gizinski84@gmail.com>
2022-05-29 20:27:10 +0200

    chunks cleaned

--- scripts/s2orc/extract_data_spacy.py
+++ scripts/s2orc/extract_data_spacy.py
@@ -5,7 +5,7 @@ import typer
 from typing import List
 tqdm.pandas()
 
-COLNAMES =  ['paper_id', 'doc_lens', 'nouns', 'noun_chunks', 'lemmas']
+COLNAMES =  ['paper_id', 'doc_lens', 'nouns', 'noun_chunks', 'lemmas', 'noun_chunks_cleaned']
 
 stopwords = set(["machine", "learning", "ai", "artificial", "intelligence"])
 
@@ -19,6 +19,19 @@ def get_chunks(doc, remove_stop_and_numbers = True):
       else:
         noun_chunks.append(chunk.text)
     return noun_chunks
+
+
+def get_chunks_cleaned(doc):
+    """Get list of noun chunks present in doc, cleaned"""
+    chunks = []
+    for chunk in doc.noun_chunks:
+      if not all([w.is_stop or (not w.is_alpha) or w.is_punct for w in chunk]):
+        if chunk[0].pos_ in ['DET','PRON']:
+          chunk = chunk[1:]
+        chunk = [t for t in chunk if t.is_alpha if not t.is_punct if not t.is_space if not t.pos_ in ['SYM', 'NUM']]
+        chunk_content = " ".join([w.lemma_.lower() for w in chunk])
+        chunks.append(chunk_content)
+    return chunks
 
 
 def get_nouns(doc)->List[str]:
@@ -41,14 +54,6 @@ def main(in_path: str, out_path:str, batch_size=1000, spacy_model_name: str='en_
     en = spacy.load(spacy_model_name)
     en.remove_pipe("ner") # removing entity recognition for speed
 
-    def process(text:str):
-        try:
-            return en(text)
-        except Exception:
-            print("Error: {}".format(text))
-            return []
-    error = 0
-
     try:
       out_df = pd.read_parquet(out_path)
       print("Loaded existing data:", len(out_df))
@@ -61,9 +66,11 @@ def main(in_path: str, out_path:str, batch_size=1000, spacy_model_name: str='en_
     df = df[~df['paper_id'].isin(out_df['paper_id'])]
     print("Number of papers not processed yet:", len(df))
     b_results=[]
-    for paper_id, doc in zip(df['paper_id'], en.pipe(df['abstract'])):
+    for paper_id, doc in zip(df['paper_id'], en.pipe(df['abstract'], n_process=-1)):
       try:
-        b_results.append([paper_id, len(doc), get_nouns(doc), get_chunks(doc), [t.lemma_ for t in doc]])
+        chunks = get_chunks(doc)
+        chunks_cleaned = get_chunks_cleaned(doc)
+        b_results.append([paper_id, len(doc), get_nouns(doc), chunks, [t.lemma_ for t in doc],chunks_cleaned])
       except Exception as e:
         error += 1
         print("Error: {}".format(e))
