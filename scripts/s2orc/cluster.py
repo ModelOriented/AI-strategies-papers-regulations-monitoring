c2b7dd7d68e0652b3abae4729d7d7aea956f3f5a
Stanisław Giziński <s.gizinski84@gmail.com>
2022-06-01 15:46:12 +0200

    fix clustering

--- scripts/s2orc/cluster.py
+++ scripts/s2orc/cluster.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import hdbscan
 import typer
 import os
+import numpy as np
 
-
-def main(in_json_path: str, out_path: str, n_jobs:int = -1, cluster_selection_epsilons:str=".0", use_gpu:bool=False):
+def main(in_parquet_path: str, out_path: str, n_jobs:int = -1, cluster_selection_epsilons:str=".0", use_gpu:bool=False):
     if use_gpu:
         from cuml.cluster import HDBSCAN
     else:
@@ -13,14 +12,14 @@ def main(in_json_path: str, out_path: str, n_jobs:int = -1, cluster_selection_ep
     epsilons = [float(epsilon) for epsilon in cluster_selection_epsilons.split(",")]
     print("Will process epsilons:", epsilons)
     print("Loading parquet...")
-    chunk_to_embedding = pd.read_parquet(in_json_path)
+    chunk_to_embedding = pd.read_parquet(in_parquet_path)
     print(len(chunk_to_embedding))
     
     for eps in epsilons:
         model = HDBSCAN(core_dist_n_jobs=n_jobs, cluster_selection_epsilon=eps)
         print("epsilon = ", eps)
-        # print("Clutering chunks...")
-        clusters = model.fit_predict(chunk_to_embedding['embedding'])
+        print("Clutering chunks...")
+        clusters = model.fit_predict(np.stack(chunk_to_embedding['embedding']))
         # print("Saving data...")
         df_out = pd.DataFrame([{"cluster": cluster, "chunk":chunk, "embedding":embedding} for cluster, (chunk, embedding) in zip(clusters, chunk_to_embedding.items())])
         print("Outliers number:", sum(df_out['cluster']==-1), "which is", sum(df_out['cluster']==-1)/len(df_out))
