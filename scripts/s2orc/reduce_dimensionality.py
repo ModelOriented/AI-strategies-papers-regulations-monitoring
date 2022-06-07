a81cbabd9a25c56492067f6d313ef4c411bece52
Stanislaw Gizinski <s.gizinski84@gmail.com>
2022-06-07 22:12:26 +0200

    add gpu umap

--- scripts/s2orc/reduce_dimensionality.py
+++ scripts/s2orc/reduce_dimensionality.py
@@ -5,15 +5,22 @@ import typer
 
 import numpy as np
 
-def main(n_components:int):
+def main(n_components:int, gpu:bool=False):
     print("Reducing dimensionality to {} components".format(n_components))
     print("Loading data...")
+    if gpu:
+        print("Using GPU")
+        import cuml
+        reducer = cuml.UMAP(n_components=n_components, random_state=42)
+    else:
+        print("Using CPU")
+        reducer = umap.UMAP(n_components=n_components, random_state=42)
+
     df = pd.read_parquet('data/s2orc/embeddings/big_cleaned_mini_all-MiniLM-L6-v2.parquet')
     out = f'data/s2orc/embeddings/reduced_{n_components}_big_cleaned_mini_all-MiniLM-L6-v2.parquet'
 
     emb = df['embedding']
 
-    reducer = umap.UMAP(n_components=n_components, random_state=42)
     print("Reducing...")
     reduced_embeddings = reducer.fit_transform(list(emb))
 
