95e6830318cbbf32110cc3322aad3c84c89fc36e
Stanislaw Gizinski <s.gizinski84@gmail.com>
2022-06-11 14:00:59 +0200

    analyse clusters

--- scripts/s2orc/analyse_clusterings.py
+++ scripts/s2orc/analyse_clusterings.py
@@ -9,9 +9,11 @@ import os.path
 rows = []
 for path in glob('data/s2orc/clusterings/*.parquet'):
     print(path)
-    df = pd.read_parquet(path)
+    df = pd.read_parquet(path,columns=['chunk', 'cluster'])
     outliers =(df['cluster']==-1).sum()
-    rows.append({"name": os.path.basename(path), "n_clusters": df['cluster'].nunique(), "n_outliers": outliers})
+    n_clusters = df['cluster'].nunique()-1
+    mean_cluster_size = (df['cluster']==-1).sum()/n_clusters
+    rows.append({"name": os.path.basename(path), "n_clusters": n_clusters, "n_outliers": outliers})
 
 print(rows)
 
