63d68b5d1e140cee49969b22726bcfd0349bb177
emilia_wisnios <wisniosemilia@gmail.com>
2022-06-11 14:00:00 +0200

    fix plots

--- scripts/clusters_visualisation.py
+++ scripts/clusters_visualisation.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import cuml
 
 def main(input_path:str,output_path:str):
+    print("Data loading ...")
     #reading in data
     df = pd.read_parquet(input_path)
     #clearing data - in current dataset also 35 cluster with perccentages
@@ -26,13 +27,15 @@ def main(input_path:str,output_path:str):
     df_clear['cluster_name'] = [clusters.loc[(int(row['cluster'])),'name'] for i, row in df_clear.iterrows()]
 '''
     #getting the embedding to a Dataframe
-    final_rows = []
-    for _, row in df_clear.iterrows():
-        embed=row['embedding']
-        final_rows.append(embed)
-    df2 = pd.DataFrame(final_rows)
+    # final_rows = []
+    # for _, row in df_clear.iterrows():
+    #     embed=row['embedding']
+    #     final_rows.append(embed)
+    # df2 = pd.DataFrame(final_rows)
+    df2 = df_clear['embedding']
 
     #UMAP
+    print('UMAP ...')
     reducer = cuml.UMAP(
         n_neighbors=20, n_components=2, min_dist=0.0, target_metric="cosine"
     )
@@ -40,10 +43,11 @@ def main(input_path:str,output_path:str):
     result = pd.DataFrame(umap_data, columns=["x", "y"])
     df_clear['x'] = umap_data[:,0]
     df_clear['y'] = umap_data[:,1] 
-
+    print('Ploting ...')
     # Visualize clusters
     fig = px.scatter(df_clear, x='x', y='y', color='cluster_name', hover_data=['chunk'],
                     hover_name='cluster_name')
+    print('Saving ...')
     fig.write_html(output_path+'.html')
     fig.show()
 
