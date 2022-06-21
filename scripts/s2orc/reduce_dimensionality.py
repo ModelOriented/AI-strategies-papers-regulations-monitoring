8dd67571521c39046f0ac2e7b7e3e76197c8484f
Stanislaw Gizinski <s.gizinski84@gmail.com>
2022-06-21 13:46:06 +0200

    reduce dimensionality rewrite

--- scripts/s2orc/reduce_dimensionality.py
+++ scripts/s2orc/reduce_dimensionality.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import typer
+import os
 
 
+EMBEDDINGS_DIR_PATH = "data/s2orc/embeddings/"
 import numpy as np
 
-def main(n_components:int, gpu:bool=False):
+def main(file_name:str,n_components:int, gpu:bool=False):
     print("Reducing dimensionality to {} components".format(n_components))
-    print("Loading data...")
+    print("Loading libraries...")
     if gpu:
         print("Using GPU")
         import cuml
@@ -15,9 +17,14 @@ def main(n_components:int, gpu:bool=False):
         print("Using CPU")
         import umap
         reducer = umap.UMAP(n_components=n_components, random_state=42)
-
-    df = pd.read_parquet('data/s2orc/embeddings/big_cleaned_mini_all-MiniLM-L6-v2.parquet')
-    out = f'data/s2orc/embeddings/reduced_{n_components}_big_cleaned_mini_all-MiniLM-L6-v2.parquet'
+    
+    in_file_path = os.path.join(EMBEDDINGS_DIR_PATH, file_name)
+    out_file_name = f"reduced_{str(n_components)}_{file_name}"
+    out_file_path = os.path.join(EMBEDDINGS_DIR_PATH, out_file_name)
+    print("Loading from:",in_file_path)
+    print("Will save to:",out_file_path)
+    print("Loading...")
+    df = pd.read_parquet(in_file_path)
 
     emb = df['embedding']
 
@@ -26,8 +33,8 @@ def main(n_components:int, gpu:bool=False):
 
     df['embedding'] = list(reduced_embeddings)
     print("Saving...")
-    df.to_parquet(out)
+    df.to_parquet(out_file_path)
 
-
+    print("Done!")
 if __name__=="__main__":
     typer.run(main)
