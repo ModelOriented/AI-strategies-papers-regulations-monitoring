940d12b7ec65a92034359fe4200e0e07b5a9b47b
emilia_wisnios <wisniosemilia@gmail.com>
2022-06-24 12:12:50 +0200

    paths fix

--- scripts/s2orc/match_embedding_to_cluster.py
+++ scripts/s2orc/match_embedding_to_cluster.py
@@ -8,8 +8,8 @@ import scipy
 import typer
 
 
-def main(filename:str, sentences_embedding:str, multiprocess:bool, batch_size:int):
-    ai_act_chunks = pd.read_csv(os.path.join('data', 's2orc', 'ai_act', filename))
+def main(sentences_embedding:str, multiprocess:bool, batch_size:int):
+    ai_act_chunks = pd.read_csv('data/s2orc/chunks_from_ai_act1.csv.dvc')
     all_chunks = list(set(ai_act_chunks['chunk']))
 
     print('Embedding ...')
@@ -29,13 +29,13 @@ def main(filename:str, sentences_embedding:str, multiprocess:bool, batch_size:in
 
     print('UMAP ...')
 
-    reducer = joblib.load(os.path.join('data', 's2orc', 'embeddings', 'umap_reducers', filename))
+    reducer = joblib.load('data/s2orc/embeddings/umap_reducers/big_cleaned_phrase-bert.parquet')
     reduced_embeddings = reducer.transform(np.stack(embeddings))
 
     df['reduced_embedding'] = list(reduced_embeddings)
 
     print('Finding nearest points ...')
-    base_embeddings = pd.read_parquet(os.path.join('data', 's2orc', 'embeddings', filename))
+    base_embeddings = pd.read_parquet('data/s2orc/embeddings/reduced_300_big_cleaned_phrase-bert.parquet')
 
     Tree = scipy.spatial.cKDTree(base_embeddings['embedding'], leafsize=100)
     closest_idx = []
@@ -46,10 +46,10 @@ def main(filename:str, sentences_embedding:str, multiprocess:bool, batch_size:in
 
     print('Looking for closest clusters ...')
 
-    clusts = pd.read_parquet(os.path.join('data', 's2orc', 'clusterings', filename), columns=['meme_id'])
-    clusts.reset_index()
+    clusts = pd.read_parquet('data/s2orc/clusterings/reduced_300_big_cleaned_phrase-bert_eps_0.2_min_clust_size_3.parquet', columns=['meme_id'])
+    clusts.reset_index(inplace=True)
     df = df.merge(clusts, left_on='closest_idx', right_on='index')
-    df.to_parquet(os.path.join('data', 's2orc', 'clusterings', 'ai_act', filename))
+    df.to_parquet('data/s2orc/clusterings/ai_act/reduced_300_big_cleaned_phrase-bert_eps_0.2_min_clust_size_3.parquet')
 
 if __name__ == '__main__':
     typer.run(main)
