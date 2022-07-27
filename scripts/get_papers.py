c775cb2f3fed53eaa7d149a2e6cee08b14d2bee4
emilia_wisnios <wisniosemilia@gmail.com>
2022-07-27 21:26:36 +0200

    search for keywords directly in inverted index

--- scripts/get_papers.py
+++ scripts/get_papers.py
@@ -2,24 +2,16 @@ import os
 import jsonlines
 import pandas as pd
 import typer
-from spacy.matcher import PhraseMatcher
-import spacy.lang.en
 
-ROOT_DIR = '/raid/shared/mair/AI-strategies-papers-regulations-monitoring/openalex-snapshot/works'
+ROOT_DIR = 'openalex-snapshot/works'
 
-ML_KEYWORDS = ['artificial intelligence', 'machine learning', 'classifier', 'neural network', 'deep learning',
-               'data science', 'nlp', 'computer vision', 'ai', 'neural net', 'natural language processing', 'cnn',
-               'rnn', 'lstm', 'backpropagation', 'reinforcement learning', 'xgboost', 'random forest', 'svm',
-               'decision tree', 'gradient boosting', 'bayesian network']
-
-en = spacy.lang.en.English()
-
-
-def prepare_matcher():
-    matcher = PhraseMatcher(en.vocab, attr="LEMMA")
-    for phrase in ML_KEYWORDS:
-        matcher.add(phrase, None)
-    return matcher
+ML_KEYWORDS = ['artificial intelligence', 'neural network', 'machine learning', 'expert system',
+               'natural language processing', 'deep learning', 'reinforcement learning', 'learning algorithm',
+               'supervised learning', 'unsupervised learning' , 'intelligent agent', 'backpropagation learning',
+               'backpropagation algorithm', 'long short term memory', 'autoencoder', 'q learning', 'feedforward net',
+               'xgboost', 'transfer learning', 'gradient boosting', 'generative adversarial network',
+               'representation learning', 'random forest', 'support vector machine', 'multiclass classification',
+               'robot learning', 'graph learning', 'naive bayes classification', 'classification algorithm']
 
 
 def get_abstract(abstract_inverted_index: dict) -> str:
@@ -34,20 +26,15 @@ def get_abstract(abstract_inverted_index: dict) -> str:
 
 def main(output_dir: str):
     ml_papers = []
-    matcher = prepare_matcher()
     for subdir, dirs, files in os.walk(ROOT_DIR):
         for file in files:
             with jsonlines.open(file) as reader:
                 for object in reader:
-                    abstract = get_abstract(object['abstract_inverted_index'])
-                    to_keep = False
-                    if abstract:
-                        doc = en(abstract)
-                        matches = matcher(doc)
-                        if len(matches) > 0:
-                            to_keep = True
-                    if to_keep:
-                        ml_papers.append(object)
+                    for keyword in ML_KEYWORDS:
+                        words = keyword.split()
+                        if all(word in object['abstract_inverted_index'] for word in words):
+                            ml_papers.append(object)
+                            break
 
     df = pd.DataFrame(ml_papers)
     df.to_parquet(output_dir)
