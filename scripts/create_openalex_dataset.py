5117f19918f4a1faa727da0f652cae37450db3da
emilia_wisnios <wisniosemilia@gmail.com>
2022-08-02 09:56:24 +0200

    bug fix

--- scripts/create_openalex_dataset.py
+++ scripts/create_openalex_dataset.py
@@ -6,8 +6,10 @@ import json
 
 def create_openalex_dataset(path_to_filtered_files:str, output_dir:str):
     first = True
+    i = 1
     for subdir, dirs, files in os.walk(path_to_filtered_files):
         for file in files:
+            print(f'{i}/{1330668}', flush=True)
             if file != 'already_processed.txt':
                 full_path = os.path.join(subdir, file)
                 with open(full_path) as f:
@@ -17,7 +19,7 @@ def create_openalex_dataset(path_to_filtered_files:str, output_dir:str):
                     else:
                         data_temp = pd.DataFrame(json.loads(line) for line in f)
                         data = data.append(data_temp, ignore_index=True)
-    data.to_parquet(os.path.join(output_dir, 'openalex_ml_dataset.parquet'))
+            data.to_parquet(os.path.join(output_dir, 'openalex_ml_dataset.parquet'))
 
 
 if __name__ == '__main__':
