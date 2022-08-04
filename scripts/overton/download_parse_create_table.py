e5a51ca39d5cb50407aadcc06e4b44b203de2470
emilia_wisnios <wisniosemilia@gmail.com>
2022-08-04 16:17:25 +0200

    Hubert fix

--- scripts/overton/download_parse_create_table.py
+++ scripts/overton/download_parse_create_table.py
@@ -235,7 +235,7 @@ def merge_tables(meta, subtable):
             huge_table['n_words'][idx] = subtable['n_words'][i]
         except Exception as e:
             print(e)
-            print(subtable['Name'][i])
+            print(subtable['Name'][i].strip('.pdf'))
             print(huge_table.index[huge_table['Title'] == subtable['Name'][i].strip('.pdf')])
             print(j)
             j += 1
