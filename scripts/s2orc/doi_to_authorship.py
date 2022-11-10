2636573b006173a7a103be0abc5e37e6ec12567a
Kaczyniec <kaczynska.pm@gmail.com>
2022-11-10 17:15:20 +0100

    to make dict from openalex

---
+++ scripts/s2orc/doi_to_authorship.py
@@ -0,0 +1,24 @@
+import json
+
+import os
+
+def doi_to_authorship(directory_path:str):
+
+    directory = os.fsencode(directory_path)
+    
+    doi_to_authorship = {}
+
+    for update_dir in os.listdir(directory):
+        for file in os.listdir(update_dir):
+
+            oa_file = open(file)
+            oa = json.load(oa_file)
+
+            for key in oa:
+                doi_to_authorship[key['doi']] = key['author']
+    return doi_to_authorship
+
+if __name__ == "__main__":
+    doi_dict = doi_to_authorship('/raid/shared/openalex/openalex-snapshot/data/works')
+    with open('doi_to_authorship_extended.json', 'w') as fp:
+        json.dump(doi_dict, fp)
