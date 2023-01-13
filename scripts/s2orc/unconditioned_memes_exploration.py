5a82bf62b9ece87679829c62f00f3a2e6f310bad
Kaczyniec <kaczynska.pm@gmail.com>
2023-01-14 00:58:27 +0100

    corrections with memory

--- scripts/s2orc/unconditioned_memes_exploration.py
+++ scripts/s2orc/unconditioned_memes_exploration.py
@@ -4,15 +4,16 @@ import memes_pipeline
 
 if __name__ == '__main__':
     
-    clustering_path = ['/reduced_300_big_cleaned_phrase-bert_eps_0.2_min_clust_size_3.parquet',
-                        '/reduced_300_big_cleaned_phrase-bert_eps_0.3_min_clust_size_3.parquet',
-                        '/reduced_300_big_cleaned_phrase-bert_eps_0.4_min_clust_size_3.parquet']
+    clustering_path = ['/reduced_300_big_cleaned_phrase-bert_eps_0.0_min_clust_size_3.parquet',
+                        '/reduced_300_big_cleaned_phrase-bert_eps_0.1_min_clust_size_3.parquet',
+                        '/reduced_300_big_cleaned_phrase-bert_eps_0.0_min_clust_size_5.parquet',
+                        '/reduced_300_big_cleaned_mini_all-MiniLM-L6-v2_eps_0.0_min_clust_size_3.parquet']
     for path in clustering_path:
 
         memes_pipeline.pipeline(None,
-                'data/s2orc/chunk_meme_mappings'+path, 
+                'data/s2orc/clusterings'+path, 
                 'data/s2orc/meme_score'+path, 
                 aff_output_path = 'data/s2orc/big_ai_dataset_with_affiliations_extended_oa.parquet',
-                condition_list = ['company'],
-                category = 'types',
+                condition_list = [],
+                category = None,
                 do_cluster = False)
